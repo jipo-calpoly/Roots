70acb26ff360e75da144c50e0ecc75d834ae55d7
Jill Polsin <jpolsin@calpoly.edu>
2020-02-08 13:42:26 -0800

    Initial Commit

---
+++ Roots/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Roots
+//
+//  Created by Jill Polsin on 2/8/20.
+//  Copyright © 2020 Roots. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
